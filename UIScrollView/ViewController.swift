6d26c6f929813af77f77ffaa34729c1164c89a3d
William Yu <wyu@danteinc.com>
2016-06-08 13:21:02 -0400

    initial commit

---
+++ UIScrollView/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  UIScrollView
+//
+//  Created by Yu, William on 6/8/16.
+//  Copyright © 2016 iwllyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        label.text = "Mi elementum facilisis vestibulum ut parturient quisque nibh curabitur nullam mauris nec a consectetur tincidunt adipiscing a eu vel taciti ullamcorper penatibus porta a. Nec consectetur leo lobortis a parturient suspendisse suspendisse purus tempus malesuada est a non fermentum parturient posuere natoque natoque quam adipiscing diam mollis consectetur a scelerisque. Cum adipiscing condimentum enim velit varius a at a malesuada ultricies parturient massa mi condimentum. Elit suspendisse et id scelerisque donec urna sem augue ornare condimentum parturient dolor a pharetra vestibulum vestibulum non hac parturient ultrices parturient lectus nulla nullam dignissim odio. Montes nibh condimentum etiam parturient fringilla commodo elit laoreet parturient nisl condimentum penatibus quam dis a adipiscing nisl."
+//
+//        + "Condimentum ante nam parturient phasellus orci ac mus convallis a placerat ullamcorper fames vulputate sem. Platea vestibulum adipiscing per elit parturient magna nulla mi vestibulum parturient volutpat duis odio vestibulum erat rhoncus ad vestibulum venenatis etiam parturient rutrum et duis leo velit. Sem lorem nunc adipiscing a a sem maecenas maecenas nisl consequat sociosqu rutrum sagittis etiam consectetur neque nec. Sed ac eros rhoncus id sed nec nisi malesuada parturient nam fermentum dui a consectetur ut eleifend ligula porttitor scelerisque rhoncus blandit justo parturient a molestie.\n\n" +
+//        
+//        "Cubilia dignissim leo ac ullamcorper mattis suspendisse per vestibulum a a justo consectetur ac fermentum vestibulum suspendisse fames condimentum posuere. Mus augue at a consectetur a eros taciti hendrerit parturient parturient lectus commodo ultrices enim sem elementum tincidunt ullamcorper turpis purus hac torquent vestibulum a. Nam eleifend mi cursus ad accumsan volutpat varius est mus fusce egestas ac mi est vestibulum tellus posuere condimentum felis donec velit. Augue at dapibus dapibus ut consectetur bibendum sociosqu eu mi a vulputate suspendisse platea suspendisse urna in vestibulum. Nam condimentum parturient placerat tortor nam nascetur duis enim duis nibh scelerisque platea a elementum vel nisl praesent suspendisse condimentum torquent potenti scelerisque. Neque dolor ad magna morbi parturient montes nisl leo a vel tempor enim posuere penatibus dui lorem torquent elit at enim ultricies conubia venenatis suspendisse a a.\n\n" +
+//        
+//        "Nibh hac quisque augue suspendisse rutrum elementum netus ac a maecenas vestibulum tempus ullamcorper nulla parturient volutpat vestibulum accumsan dictumst magna lacus litora. Adipiscing sociosqu a class parturient aliquam placerat vivamus curabitur per nisi inceptos orci dolor malesuada consectetur et velit. Dapibus elit lacinia fermentum phasellus a sociosqu commodo amet imperdiet fringilla pulvinar ut dis neque a proin facilisis class inceptos habitasse bibendum condimentum vestibulum lacinia parturient suspendisse aliquam nam. Luctus ac non taciti hac nunc urna id potenti eu montes est a sed at arcu suspendisse erat ad himenaeos dis ullamcorper. At scelerisque sagittis leo condimentum erat ac lacinia sociosqu mus nunc id mus ullamcorper ad dui est suspendisse vivamus aptent mi parturient. Imperdiet curabitur nibh libero ut a accumsan cras vestibulum pretium mi erat congue habitasse et luctus habitant parturient hendrerit per montes nisl adipiscing malesuada."
+        
+  
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
